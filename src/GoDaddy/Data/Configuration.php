05a0534cf88580d7427a38ec031de5cc0d639e5e
Elena Filippova <elena.filippova@codeline.fi>
2023-05-30 10:24:53 +0300

    Remove customer_id from configuration

--- src/GoDaddy/Data/Configuration.php
+++ src/GoDaddy/Data/Configuration.php
@@ -12,7 +12,6 @@ use Upmind\ProvisionBase\Provider\DataSet\Rules;
  *
  * @property-read string $api_key Key
  * @property-read string $api_secret Secret
- * @property-read string $customer_id CustomerId
  * @property-read bool|null $sandbox Make API requests against the sandbox environment
  * @property-read bool|null $debug Whether or not to log API requests and responses
  */
@@ -23,7 +22,6 @@ class Configuration extends DataSet
         return new Rules([
             'api_key' => ['required', 'string', 'min:3'],
             'api_secret' => ['required', 'string', 'min:6'],
-            'customer_id' => ['nullable', 'string'],
             'sandbox' => ['nullable', 'boolean'],
             'debug' => ['nullable', 'boolean'],
         ]);
