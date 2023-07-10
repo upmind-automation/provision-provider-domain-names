6d0628b5c222b8a05f3d9c7da7d9e86367743a97
Harry Lewis <harry@upmind.com>
2023-07-10 17:03:53 +0100

    Re-order InternetBS configuration values

--- src/InternetBS/Data/Configuration.php
+++ src/InternetBS/Data/Configuration.php
@@ -10,8 +10,8 @@ use Upmind\ProvisionBase\Provider\DataSet\Rules;
 /**
  * InternetBS configuration.
  *
- * @property-read string $password Password
  * @property-read string $api_key API key
+ * @property-read string $password Password
  * @property-read bool|null $sandbox Make API requests against the sandbox environment
  * @property-read bool|null $debug Whether or not to log API requests and responses
  */
@@ -20,8 +20,8 @@ class Configuration extends DataSet
     public static function rules(): Rules
     {
         return new Rules([
-            'password' => ['required', 'string', 'min:3'],
             'api_key' => ['required', 'string', 'min:6'],
+            'password' => ['required', 'string', 'min:3'],
             'sandbox' => ['nullable', 'boolean'],
             'debug' => ['nullable', 'boolean'],
         ]);
