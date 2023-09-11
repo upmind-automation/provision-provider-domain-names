6b279cc77a8dffaa3a1fa2dd12f3c36501e6efc0
Harry Lewis <harry@upmind.com>
2023-09-11 17:18:18 +1000

    Update LogicBoxes getResponseErrorMessage() remove truncation logic after periods

--- src/LogicBoxes/Provider.php
+++ src/LogicBoxes/Provider.php
@@ -795,7 +795,7 @@ class Provider extends DomainNames implements ProviderInterface
         $errorMessage = trim($responseData['message'] ?? $responseData['error'] ?? 'unknown error');
 
         // only return the first sentence of the error message
-        $errorMessage = preg_replace('/\. .+$/', '', $errorMessage);
+        // $errorMessage = preg_replace('/\. .+$/', '', $errorMessage); // problematic if input string contains a .
 
         // neaten up validation errors
         if (preg_match('/^\{\w+=([^}]+)\}$/', $errorMessage)) {
