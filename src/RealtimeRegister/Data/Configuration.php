f6a24430f85e4503ba6f00e0e14792840bcf3ddf
Roussetos Karafyllakis <35-roussks@users.noreply.git.upmind.io>
2024-05-28 13:14:18 +0100

    Update Realtime Register Provider, removed debug flag, added throw tags, fixed phpstan

--- src/RealtimeRegister/Data/Configuration.php
+++ src/RealtimeRegister/Data/Configuration.php
@@ -13,7 +13,6 @@ use Upmind\ProvisionBase\Provider\DataSet\Rules;
  * @property-read string $customer Customer
  * @property-read string $api_key API key
  * @property-read bool|null $sandbox Make API requests against the sandbox environment
- * @property-read bool|null $debug Whether or not to log API requests and responses
  */
 class Configuration extends DataSet
 {
@@ -23,7 +22,6 @@ class Configuration extends DataSet
             'customer' => ['required', 'string'],
             'api_key' => ['required', 'string'],
             'sandbox' => ['nullable', 'boolean'],
-            'debug' => ['nullable', 'boolean'],
         ]);
     }
 }
